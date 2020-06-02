1a0f77d5abdddceafd6ca9cb5aac5b0112d6092f
maziyank <maziyank@gmail.com>
2020-06-02 19:07:53 +0700

    update style

--- src/Disclaimer.svelte
+++ src/Disclaimer.svelte
@@ -1,37 +1,69 @@
 <style>
-.content{
+  .content {
     text-align: left;
     font-size: large;
-}
+  }
 
-.content h2{
+  .content h2 {
     text-align: center;
-}
+  }
 
-.content li{
+  @media (max-width: 640px) {
+    .content {
+      margin-left: 2%;
+      margin-right: 2%;
+    }
+  }
+
+  @media (min-width: 640px) {
+    .content {
+      margin-left: 30%;
+      margin-right: 30%;
+    }
+  }
+
+  .content li {
     margin-bottom: 10px;
-    margin-left: 30%;
-    margin-right: 30%;
-}
+  }
 
-a {
+  a {
     font-weight: bold;
     color: #ff3e00;
-}
-
+  }
 </style>
 
 <div class="content">
   <h2>Disclaimer</h2>
   <ol>
     <li>
-      Data diambil dari kaggle mengenai Indonesia College Examination 2019 <a href="https://www.kaggle.com/ekojsalim/indonesia-college-entrance-examination-utbk-2019">di sini</a>. Ada sekitar 147ribu sampel (dari total skor 1,1 juta) dan data ini tidak menunjukkan seluruh set data 1,1 juta karena dikumpulkan dari sumber pihak ketiga (dengan mungkin beberapa data tidak valid berserakan). Sehingga info yang disajikan tidak dapat disebut mewakili keseluruhan keterjadian
+      Data diambil dari kaggle mengenai Indonesia College Examination 2019
+      <a
+        href="https://www.kaggle.com/ekojsalim/indonesia-college-entrance-examination-utbk-2019">
+        di sini
+      </a>
+      . Ada sekitar 147ribu sampel (dari total skor 1,1 juta) dan data ini tidak
+      menunjukkan seluruh set data 1,1 juta karena dikumpulkan dari sumber pihak
+      ketiga (dengan mungkin beberapa data tidak valid berserakan). Sehingga
+      info yang disajikan tidak dapat disebut mewakili keseluruhan keterjadian
     </li>
     <li>
-      Pengolahan data sepenuhnya dilakukan dalam rangka edukasi pemanfaatan data science dalam mengolah data, baik dari tahapan penyiapan data hingga penyajian informasi. Oleh karena itu, jangan menjadikan informasi ini sebagai dasar pengambilan keputusan apapun. Tetap gunakan informasi pada website <a href="https://ltmpt.ac.id/">LTMPT</a> sebagai rujukan resmi. 
+      Pengolahan data sepenuhnya dilakukan dalam rangka edukasi pemanfaatan data
+      science dalam mengolah data, baik dari tahapan penyiapan data hingga
+      penyajian informasi. Oleh karena itu, jangan menjadikan informasi ini
+      sebagai dasar pengambilan keputusan apapun. Tetap gunakan informasi pada
+      website
+      <a href="https://ltmpt.ac.id/">LTMPT</a>
+      sebagai rujukan resmi.
     </li>
     <li>
-      Passing grade dihitung dari nilai terendah peserta yang diterima di jurusan sesuai kuota (asumsi 20% dari kapasitas). Apakah peserta diterima atau tidak ditentukan dengan cara merangking 20% nilai rata-rata tertinggi. Semakin tinggi passing grade, semakin tinggi nilai yang dibutuhkan untuk pelamar agar lolos di jurusan tersebut. Sementara itu, acceptance ratio dihitung dari persentase pelamar yang diterima (20% dari kapasitas) dibagi dengan jumlah pelamar. Semakin kecil nilainya, semakin kecil peluang untuk diterima, begitu juga sebaliknya.
+      Passing grade dihitung dari nilai terendah peserta yang diterima di
+      jurusan sesuai kuota (asumsi 20% dari kapasitas). Apakah peserta diterima
+      atau tidak ditentukan dengan cara merangking 20% nilai rata-rata
+      tertinggi. Semakin tinggi passing grade, semakin tinggi nilai yang
+      dibutuhkan untuk pelamar agar lolos di jurusan tersebut. Sementara itu,
+      acceptance ratio dihitung dari persentase pelamar yang diterima (20% dari
+      kapasitas) dibagi dengan jumlah pelamar. Semakin kecil nilainya, semakin
+      kecil peluang untuk diterima, begitu juga sebaliknya.
     </li>
   </ol>
 </div>
